cba61d68e0c0c732e2935e0627e64020ba2c40fb
Christoph Waibel <christophwaibel@gmx.de>
2019-12-09 14:23:06 +0100

    more cleaning up

---
+++ MetaheuristicsTuner/HyperParameterClassifier.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using ILOG.CPLEX;
+using ILOG.Concert;
+
+
+namespace MetaheuristicsTuner
+{
+    internal static class HyperParameterClassifier
+    {
+        /// <summary>
+        /// Getting maximum congruency for naming parameter sets A or B. Linear Programming optimization model.
+        /// Eq. (4) and Table II in DOI:10.1109/CEC.2019.8790261
+        /// The matrices need to be manually entered here in the script (cTopA, cTopB, cBtmA, cBtmB)
+        /// </summary>
+        internal static void IdentifyTwoBestParameterSets(string caseSolver)
+        {
+            Cplex cpl = new Cplex();
+
+            int N = 7;
+            //string caseSolver = "ES"; //"SGA" (default), "ES", "PSO", "FIPS" 
+
+            int[] cTopA;
+            int[] cTopB;
+            int[] cBtmA;
+            int[] cBtmB;
+            switch (caseSolver)
+            {
+                default:
+                    cTopA = new int[] { 11, 1, 1, 13, 0, 0, 2 };
+                    cTopB = new int[] { 0, 5, 3, 0, 5, 5, 5 };
+                    cBtmA = new int[] { 2, 12, 12, 0, 14, 14, 9 };
+                    cBtmB = new int[] { 4, 0, 1, 4, 0, 0, 0 };
+                    break;
+                case "ES":
+                    cTopA = new int[] { 4, 6, 9, 3, 11, 11, 5 };
+                    cTopB = new int[] { 2, 3, 1, 3, 1, 3, 3 };
+                    cBtmA = new int[] { 4, 5, 2, 8, 0, 0, 6 };
+                    cBtmB = new int[] { 3, 1, 3, 2, 5, 3, 2 };
+                    break;
+                case "PSO":
+                    cTopA = new int[] { 1, 3, 2, 3, 3, 7, 5 };
+                    cTopB = new int[] { 12, 11, 3, 1, 7, 8, 5 };
+                    cBtmA = new int[] { 7, 5, 6, 5, 5, 1, 3 };
+                    cBtmB = new int[] { 0, 1, 9, 11, 5, 4, 7 };
+                    break;
+                case "FIPS":
+                    cTopA = new int[] { 6, 6, 7, 3, 5, 0, 8 };
+                    cTopB = new int[] { 4, 6, 6, 9, 5, 9, 1 };
+                    cBtmA = new int[] { 4, 4, 3, 7, 5, 10, 2 };
+                    cBtmB = new int[] { 6, 4, 4, 1, 5, 1, 9 };
+                    break;
+            }
+
+
+            INumVar[] xTopA = new INumVar[N];
+            INumVar[] xBtmB = new INumVar[N];
+            INumVar[] xTopB = new INumVar[N];
+            INumVar[] xBtmA = new INumVar[N];
+            ILinearNumExpr value = cpl.LinearNumExpr();
+            for (int n = 0; n < N; n++)
+            {
+                xTopA[n] = cpl.BoolVar();
+                xBtmB[n] = cpl.BoolVar();
+                xTopB[n] = cpl.BoolVar();
+                xBtmA[n] = cpl.BoolVar();
+                cpl.AddEq(cpl.Sum(xTopB[n], xTopA[n]), 1);
+                cpl.AddEq(cpl.Sum(xBtmA[n], xBtmB[n]), 1);
+                cpl.AddEq(cpl.Sum(xTopA[n], xBtmA[n]), 1);
+                cpl.AddEq(cpl.Sum(xTopB[n], xBtmB[n]), 1);
+                value.AddTerm(xTopA[n], cTopA[n]);
+                value.AddTerm(xTopB[n], cTopB[n]);
+                value.AddTerm(xBtmA[n], cBtmA[n]);
+                value.AddTerm(xBtmB[n], cBtmB[n]);
+            }
+
+            cpl.AddMaximize(value);
+            cpl.Solve();
+
+            Console.WriteLine("Parameter Grouping for Solver: {0}", caseSolver);
+            for (int n = 0; n < N; n++)
+            {
+                Console.WriteLine("n: {0}", n);
+                Console.WriteLine("xtopA: ;{0};, _____, xTopB: ;{1};", cpl.GetValue(xTopA[n]), cpl.GetValue(xTopB[n]));
+                Console.WriteLine("xbtmB: ;{0};, _____, xBtmA: ;{1};", cpl.GetValue(xBtmB[n]), cpl.GetValue(xBtmA[n]));
+            }
+            Console.WriteLine("cost: {0}", cpl.GetObjValue());
+            Console.ReadKey();
+
+
+
+
+        }
+
+    }
+}
---
+++ MetaheuristicsTuner/HyperParameterOptimization.cs
@@ -0,0 +1,220 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using MetaheuristicsLibrary.SingleObjective;
+using System.IO;
+
+
+namespace MetaheuristicsTuner
+{
+    internal static class HyperParameterOptimization
+    {
+        /// <summary>
+        /// Tuning hyper-parameters of a solver
+        /// </summary>
+        /// <param name="solver">Options: SGA, ES, FIPSO, PSO</param>
+        /// <param name="testfuncdim">dimensionality n</param>
+        /// <param name="evalbudgetmultilp">evaluation budget per test function</param>
+        internal static void TuneSolver(string solver, int testfuncdim, int evalbudgetmultilp)
+        {
+            //string solver = "PSO";               //choosing solver to be tuned. string: "SGA", "ES", "FIPSO", "SA"
+            int maxfunc = 5000;                  //func calls of the meta-optimizer. 5000
+                                                 //int testfuncdim = 20;                //tuned to this n
+            int rerunsMeta = 10;                 // 10
+            int rerunsTestFuncs = 30;            // 30
+
+
+            HyperParameterFunctions hf = new HyperParameterFunctions(testfuncdim, rerunsTestFuncs, solver, evalbudgetmultilp);   //problem dim; reruns of testfuncs; solver to be tuned
+            int[] seeds_in = new int[rerunsMeta]; //reruns of meta-optimizer
+            for (int i = 0; i < seeds_in.Length; i++) seeds_in[i] = i;
+
+            int dvar;
+            double[] ub, lb;
+            bool[] xint;
+            hf.getXandBounds(out dvar, out lb, out ub, out xint, solver);
+
+
+            Console.WriteLine();
+            Console.WriteLine(@"///////////////////////////////////////");
+            Console.WriteLine("Hyperparameter optimization of...: {0}", solver);
+            Console.WriteLine(@"tuned to n = {0}", testfuncdim);
+            Console.WriteLine(@"Meta-optimizers: SGA and ES. Each with {0} re-runs", seeds_in.Length);
+            Console.WriteLine(@"///////////////////////////////////////");
+            Console.WriteLine();
+            Console.WriteLine(@"Please enter an existing output path for results. E.g.: c:\n13\");
+            string basepath = Console.ReadLine();
+            Console.WriteLine();
+            Console.WriteLine(@"How many threads would you like to assign to this? Please enter an integer between 1 and 20.");
+            int maxthreads = Convert.ToInt16(Console.ReadLine());
+            Console.WriteLine();
+            Console.WriteLine("Thanks! Starting... This might take a couple of days.");
+
+
+
+
+
+            //Func<double[], double>[] manyhyperfuncs = new Func<double[], double>[1];
+            //manyhyperfuncs[0] = hf.HyperFunc_B_Perm0db;
+            //manyhyperfuncs[1] = hf.HyperFunc_B_RotHypEll;
+            //manyhyperfuncs[2] = hf.HyperFunc_L_Ackley;
+            //manyhyperfuncs[3] = hf.HyperFunc_O_PermDB;
+            //manyhyperfuncs[4] = hf.HyperFunc_L_Schwefel_Edge;
+            //manyhyperfuncs[0] = hf.HyperFunc_L_Levy_Edge;
+
+            Func<double[], double>[] manyhyperfuncs = new Func<double[], double>[20];
+            manyhyperfuncs[0] = hf.HyperFunc_B_Perm0db;
+            manyhyperfuncs[1] = hf.HyperFunc_B_RotHypEll;
+            manyhyperfuncs[2] = hf.HyperFunc_B_Sphere;
+            manyhyperfuncs[3] = hf.HyperFunc_B_SumSquares;
+            manyhyperfuncs[4] = hf.HyperFunc_B_Trid;
+            manyhyperfuncs[5] = hf.HyperFunc_L_Ackley;
+            manyhyperfuncs[6] = hf.HyperFunc_L_Griewank;
+            manyhyperfuncs[7] = hf.HyperFunc_L_Levy;
+            manyhyperfuncs[8] = hf.HyperFunc_L_Rastrigin;
+            manyhyperfuncs[9] = hf.HyperFunc_L_Schwefel;
+            manyhyperfuncs[10] = hf.HyperFunc_O_PermDB;
+            manyhyperfuncs[11] = hf.HyperFunc_O_StyblinskiTang;
+            manyhyperfuncs[12] = hf.HyperFunc_P_Zhakarov;
+            manyhyperfuncs[13] = hf.HyperFunc_V_DixonPrice;
+            manyhyperfuncs[14] = hf.HyperFunc_V_Rosenbrock;
+            manyhyperfuncs[15] = hf.HyperFunc_L_Levy_Edge;
+            manyhyperfuncs[16] = hf.HyperFunc_L_Schwefel_Edge;
+            manyhyperfuncs[17] = hf.HyperFunc_O_StyblinskiTang_Edge;
+            manyhyperfuncs[18] = hf.HyperFunc_V_DixonPrice_Edge;
+            manyhyperfuncs[19] = hf.HyperFunc_V_Rosenbrock_Edge;
+
+
+
+
+            //manyhyperfuncs[15] = hf.HyperFunc_B_RotHypEll_Edge;
+            //manyhyperfuncs[16] = hf.HyperFunc_B_Sphere_Edge;
+            //manyhyperfuncs[17] = hf.HyperFunc_B_SumSquares_Edge;
+            //manyhyperfuncs[18] = hf.HyperFunc_L_Ackley_Edge;
+            //manyhyperfuncs[19] = hf.HyperFunc_L_Griewank_Edge;
+            //manyhyperfuncs[21] = hf.HyperFunc_L_Rastrigin_Edge;
+            //manyhyperfuncs[24] = hf.HyperFunc_P_Zhakarov_Edge;
+
+
+
+
+            //meta optimizer SGA
+            Dictionary<string, object> settingsSGA = new Dictionary<string, object>();
+            settingsSGA.Add("maxgen", 10000);
+            settingsSGA.Add("popsize", 20);
+            settingsSGA.Add("elite", 1);
+            settingsSGA.Add("pcross", 1);
+            settingsSGA.Add("pmut", 0.3);
+            settingsSGA.Add("d", 1.1);
+            settingsSGA.Add("r", 0.1);
+            settingsSGA.Add("k", 6);
+
+
+            //meta optimizer FIPSO
+            Dictionary<string, object> settingsPSO = new Dictionary<string, object>();
+
+            //meta optimizer ES
+            Dictionary<string, object> settingsES = new Dictionary<string, object>();
+            settingsES.Add("popsize", 20);          // ∈ {2,...,200}
+            settingsES.Add("lambda", 10);            // ∈ {1,...,200}
+            settingsES.Add("roh", 2);               // ∈ {1,...,popsize}  . in hyperoptimization, express as percentage of lambda
+            settingsES.Add("x0sampling", 1);        // ∈ {0,1}  0=uniform, 1=gaussian
+            settingsES.Add("stepsize0", 5);       // ∈ [0.01, 10]
+            settingsES.Add("stepsize", 2.2);        // ∈ [0.01, 10]
+            settingsES.Add("tauc", 1);              // ∈ [0.01, 50]
+
+
+
+
+            Parallel.For(0, manyhyperfuncs.Length, new ParallelOptions { MaxDegreeOfParallelism = maxthreads }, hh =>
+            {
+            //for(int hh=0; hh<manyhyperfuncs.Length; hh++){
+            Console.WriteLine("started hh {0} of {1}", hh, manyhyperfuncs.Length - 1);
+                List<string> log_SA = new List<string>();
+                List<string> log_ES = new List<string>();
+                List<string> log_PSO = new List<string>();
+                List<string> log_SGA = new List<string>();
+                string str;
+                for (int iseeds = 0; iseeds < seeds_in.Length; iseeds++)
+                {
+                /////////////////////////////////////////            SGA          /////////////////////////////////////////
+                ///////////////////////////////////////////////////////////////////////////////////////////////////////////
+                GeneticAlgorithm ga = new GeneticAlgorithm(lb, ub, xint, maxfunc, manyhyperfuncs[hh], iseeds, settingsSGA);
+                    ga.solve();
+                    ga.get_fxoptimum();
+                    str = "Last call;" + Math.Round(ga.get_fxoptimum(), 4);
+                    for (int xx = 0; xx < dvar; xx++)
+                    {
+                        str += ";" + Math.Round(ga.get_Xoptimum()[xx], 5);
+                    }
+                    str += ";SGA";
+                    log_SGA.Add(str);
+                    Console.WriteLine("Metaoptimizer SGA DONE, seed {0} and hh {1}", iseeds, hh);
+
+
+                /////////////////////////////////////////            ES           /////////////////////////////////////////
+                ///////////////////////////////////////////////////////////////////////////////////////////////////////////
+                EvolutionStrategy es = new EvolutionStrategy(lb, ub, xint, maxfunc, manyhyperfuncs[hh], iseeds, settingsSGA);
+                    es.solve();
+                    es.get_fxoptimum();
+                    str = "Last call;" + Math.Round(es.get_fxoptimum(), 4);
+                    for (int xx = 0; xx < dvar; xx++)
+                    {
+                        str += ";" + Math.Round(es.get_Xoptimum()[xx], 5);
+                    }
+                    str += ";ES";
+                    log_ES.Add(str);
+                    Console.WriteLine("Metaoptimizer ES DONE, seed {0} and hh {1}", iseeds, hh);
+
+
+
+
+                ///////////////////////////////////////////            FIPSO        /////////////////////////////////////////
+                /////////////////////////////////////////////////////////////////////////////////////////////////////////////
+                //PSO pso = new PSO(lb, ub, xint, maxfunc, manyhyperfuncs[hh], iseeds, settingsPSO);
+                //pso.solve();
+                //pso.get_fxoptimum();
+                //str = "Last call;" + Math.Round(pso.get_fxoptimum(), 4);
+                //for (int xx = 0; xx < dvar; xx++)
+                //{
+                //    str += ";" + Math.Round(pso.get_Xoptimum()[xx], 5);
+                //}
+                //str += ";PSO";
+                //log_PSO.Add(str);
+                //Console.WriteLine("Metaoptimizer PSO DONE, seed {0} and hh {1}", iseeds, hh);
+            }
+
+
+            /////////////////////////////////////////            Writing          ///////////////////////////////////
+            ///////////////////////////////////////////////////////////////////////////////////////////////////////////
+            string fileName = basepath + solver + @"_hf_" + hh + "_AllSeeds.txt";
+                using (FileStream fs = new FileStream(fileName, FileMode.Append, FileAccess.Write))
+                using (StreamWriter sw = new StreamWriter(fs))
+                {
+                    for (int i = 0; i < log_SA.Count; i++)
+                        sw.WriteLine(log_SA[i]);
+                    for (int i = 0; i < log_ES.Count; i++)
+                        sw.WriteLine(log_ES[i]);
+                    for (int i = 0; i < log_SGA.Count; i++)
+                        sw.WriteLine(log_SGA[i]);
+                    for (int i = 0; i < log_PSO.Count; i++)
+                        sw.WriteLine(log_PSO[i]);
+                }
+
+
+
+                Console.WriteLine("Done hh {0} of {1}", hh, manyhyperfuncs.Length);
+            });
+            //}
+
+            //hf.printLogs(basepath, 15);
+
+            Console.WriteLine();
+            Console.WriteLine(@"///////////////////////////////////////");
+            Console.WriteLine("Done with everything, tuning the {0}", solver);
+            Console.WriteLine(@"///////////////////////////////////////");
+            Console.ReadKey();
+
+        }
+
+    }
+}
